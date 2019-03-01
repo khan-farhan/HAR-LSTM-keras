6a2c3285edb0672ee2583729c002947edf66b5c8
P439-Farhan-Khan <p439-farhan-khan@P439-Farhan-KhannoMacBook-Pro.local>
2019-03-01 20:09:47 +0900

    making some changes

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -9,21 +9,14 @@ print("")
 
 print("Downloading...")
 if not os.path.exists("Data"):
-    call(
-
-        'mkdir Data' ,
-        shell=True
-        )
+    call( 'mkdir Data' , shell=True )
     
     os.chdir(os.getcwd() + "/Data")
 
-    call(
-
-        'wget "https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI HAR Dataset.zip"',
-        shell=True
-        )
-
+    call('wget "https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI HAR Dataset.zip"',shell=True)
+    
     print("Downloading done.\n")
+
 else:
     print("Dataset already downloaded. Did not download twice.\n")
 
@@ -32,10 +25,11 @@ else:
 print("Extracting...")
 extract_directory = os.path.abspath("Data/UCI HAR Dataset")
 if not os.path.exists(extract_directory):
-    call(
-        'unzip -nq "UCI HAR Dataset.zip"',
-        shell=True
-    )
+    
+    call( 'unzip -nq "UCI HAR Dataset.zip"', shell=True )
+    
     print("Extracting successfully done to {}.".format(extract_directory))
+
 else:
+   
     print("Dataset already extracted. Did not extract twice.\n")
